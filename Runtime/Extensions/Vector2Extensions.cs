6da0a8f799984bf6bee9e2ab94773dc9147e82f8
miguel <miguelcok27@gmail.com>
2021-09-03 14:31:52 -0400

    FEAT: vector mirroring in Vector2Extensions

--- Runtime/Extensions/Vector2Extensions.cs
+++ Runtime/Extensions/Vector2Extensions.cs
@@ -25,6 +25,34 @@ namespace Utils.Runtime.Extensions
             float y = (vector.y - yMin) / height * targetHeight + targetYMin;
             return new Vector2(x, y);
         }
+
+        public static void MirrorVerticallyInPlace(this Vector2 vector, Rect bounds)
+        {
+            float verticalPercent = (vector.y - bounds.yMin) / bounds.height;
+            float mirroredY = (1 - verticalPercent) * bounds.height + bounds.yMin;
+            vector.y = mirroredY;
+        }
+        
+        public static Vector2 MirrorVertically(this Vector2 vector, Rect bounds)
+        {
+            var mirrored = new Vector2(vector.x, vector.y);
+            mirrored.MirrorVerticallyInPlace(bounds);
+            return mirrored;
+        }
+        
+        public static void MirrorHorizontallyInPlace(this Vector2 vector, Rect bounds)
+        {
+            float horizontalPercent = (vector.x - bounds.xMin) / bounds.width;
+            float mirroredX = (1 - horizontalPercent) * bounds.width + bounds.xMin;
+            vector.x = mirroredX;
+        }
+        
+        public static Vector2 MirrorHorizontally(this Vector2 vector, Rect bounds)
+        {
+            var mirrored = new Vector2(vector.x, vector.y);
+            mirrored.MirrorHorizontallyInPlace(bounds);
+            return mirrored;
+        }
         
         /// <summary>
         /// Devuelve un vector ortogonal al pasado por parámetro y de magnitud igual a 1.
