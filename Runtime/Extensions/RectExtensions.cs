1b035456a51b7a197a8f3f372feb1a36db4c19a0
miguel <miguelcok27@gmail.com>
2021-09-03 12:42:16 -0400

    FEAT: rect extensions

---
+++ Runtime/Extensions/RectExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils.Runtime.Extensions
+{
+    public static class RectExtensions
+    {
+        public static IEnumerable<float> HorizontalEvenlySplit(this Rect rect)
+        {
+            float log = Mathf.Log10(rect.width);
+            int logFloor = Mathf.FloorToInt(log);
+            log = log > logFloor + 0.5f ? logFloor : logFloor - 1;
+            float increment = Mathf.Pow(10, log);
+            float firstValue = Mathf.Ceil(rect.xMin / increment) * increment;
+            for (float i = firstValue; i <= rect.xMax; i += increment)
+            {
+                yield return i;
+            }
+        }
+        
+        public static IEnumerable<float> VerticalEvenlySplit(this Rect rect)
+        {
+            float log = Mathf.Log10(rect.height);
+            int logFloor = Mathf.FloorToInt(log);
+            log = log > logFloor + 0.5f ? logFloor : logFloor - 1;
+            float increment = Mathf.Pow(10, log);
+            float firstValue = Mathf.Ceil(rect.yMin / increment) * increment;
+            for (float i = firstValue; i <= rect.yMax; i += increment)
+            {
+                yield return i;
+            }
+        }
+    }
+}
