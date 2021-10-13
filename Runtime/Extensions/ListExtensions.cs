20ce5c23c65cd8b828b099808e46bec3bddf34ea
miguel <miguelcok27@gmail.com>
2021-10-13 11:02:21 -0400

    FIX: ListExtensions.GetRandom implemented as an extension method correctly

--- Runtime/Extensions/ListExtensions.cs
+++ Runtime/Extensions/ListExtensions.cs
@@ -5,7 +5,7 @@ namespace Utils.Runtime.Extensions
 {
     public static class ListExtensions
     {
-        public static T GetRandom<T>(List<T> list)
+        public static T GetRandom<T>(this List<T> list)
         {
             int count = list.Count;
             int index = Random.Range(0, count);
