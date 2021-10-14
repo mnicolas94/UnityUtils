7cdeedae32c66292b102c1cb5855f0155cf0ff71
miguel <miguelcok27@gmail.com>
2021-10-14 16:02:52 -0400

    FEAT: implemented Vector3IntExtensions

---
+++ Runtime/Extensions/Vector3IntExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils.Runtime.Extensions
+{
+    public static class Vector3IntExtensions
+    {
+        public static int ManhattanDistance(this Vector3Int a, Vector3Int b)
+        {
+            var diff = a - b;
+            int distance = Math.Abs(diff.x) + Math.Abs(diff.y);
+            return distance;
+        }
+    }
+}
