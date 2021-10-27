0b346c02667f3aaa6f289671bc1be4694164f58b
miguel <miguelcok27@gmail.com>
2021-10-27 12:36:17 -0400

    FEAT: added coroutine utils

---
+++ Runtime/CoroutineUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils.Runtime
+{
+    public static class CoroutineUtils
+    {
+        public static IEnumerator CoroutineSequence(List<IEnumerator> coroutines)
+        {
+            foreach (var coroutine in coroutines)
+            {
+                yield return coroutine;
+            }
+        }
+        
+        public static IEnumerator ActionCoroutine(Action action, bool afterFrame=true)
+        {
+            if (afterFrame)
+                yield return null;
+        
+            action?.Invoke();
+            
+            if (!afterFrame)
+                yield return null;
+        }
+
+        public static IEnumerator WaitTimeCoroutine(float time)
+        {
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
