177b01a3cd26610e116ccbbc5e03d936329f65bb
miguel <miguelcok27@gmail.com>
2021-11-05 11:35:36 -0400

    FEAT: AddRangeIfNotExists method added to list extensions

--- Runtime/Extensions/ListExtensions.cs
+++ Runtime/Extensions/ListExtensions.cs
@@ -11,5 +11,17 @@ namespace Utils.Runtime.Extensions
             int index = Random.Range(0, count);
             return list[index];
         }
+
+        public static void AddRangeIfNotExists<T>(this List<T> list, List<T> range)
+        {
+            for (int i = 0; i < range.Count; i++)
+            {
+                var t = range[i];
+                if (!list.Contains(t))
+                {
+                    list.Add(t);
+                }
+            }
+        }
     }
 }
