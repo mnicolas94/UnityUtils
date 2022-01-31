83246a4e72d5e6fcd370094b667f24a83eacf156
miguel <miguelcok27@gmail.com>
2022-01-31 18:42:36 -0500

    FEAT: add image alpha hit threshold

---
+++ Runtime/UI/ImageAlphaHit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Utils.UI
+{
+    public class ImageAlphaHit : MonoBehaviour
+    {
+        [SerializeField] [Range(0.0f, 1.0f)] private float alphaThreshold;
+        private Image _image;
+    
+        private void Start()
+        {
+            _image = GetComponent<Image>();
+            UpdateThreshold();
+        }
+
+        [ContextMenu("Update threshold")]
+        public void UpdateThreshold()
+        {
+            _image.alphaHitTestMinimumThreshold = alphaThreshold;
+        }
+    }
+}
