f866c31ddb4e2b3c6cba6561ad129647315f76cb
mnicolas94 <miguelcok27@gmail.com>
2022-02-25 10:26:00 -0500

    FEAT: (coroutine utils) add function WaitCoroutine

--- Runtime/CoroutineUtils.cs
+++ Runtime/CoroutineUtils.cs
@@ -15,24 +15,6 @@ namespace Utils
             }
         }
         
-        public static IEnumerator WaitAll(MonoBehaviour owner, List<IEnumerator> coroutines)
-        {
-            int running = coroutines.Count;
-            foreach (var coroutine in coroutines)
-            {
-                owner.StartCoroutine(CoroutineSequence(new List<IEnumerator>
-                {
-                    coroutine,
-                    ActionCoroutine(() => running--, false, false)
-                }));
-            }
-
-            while (running > 0)
-            {
-                yield return null;
-            }
-        }
-        
         public static IEnumerator ActionCoroutine(
             Action action,
             bool waitPreviousFrame=true,
@@ -50,6 +32,39 @@ namespace Utils
         public static IEnumerator WaitTimeCoroutine(float time)
         {
             yield return new WaitForSeconds(time);
+        }
+        
+        public static IEnumerator WaitCoroutine(MonoBehaviour owner, IEnumerator coroutine)
+        {
+            bool running = true;
+            owner.StartCoroutine(CoroutineSequence(new List<IEnumerator>
+            {
+                coroutine,
+                ActionCoroutine(() => running = false, false, false)
+            }));
+
+            while (running)
+            {
+                yield return null;
+            }
+        }
+        
+        public static IEnumerator WaitAll(MonoBehaviour owner, List<IEnumerator> coroutines)
+        {
+            int running = coroutines.Count;
+            foreach (var coroutine in coroutines)
+            {
+                owner.StartCoroutine(CoroutineSequence(new List<IEnumerator>
+                {
+                    coroutine,
+                    ActionCoroutine(() => running--, false, false)
+                }));
+            }
+
+            while (running > 0)
+            {
+                yield return null;
+            }
         }
     }
 }
