f6a7dbb44a094f894fb5dd4410eaca7b15006389
mnicolas94 <miguelcok27@gmail.com>
2022-03-29 14:24:20 -0400

    v0.6.0: add tweening utils

---
+++ Runtime/Tweening/Curves.cs
@@ -0,0 +1,10 @@
+﻿namespace Utils.Tweening
+{
+    public static class Curves
+    {
+        public static float Linear(float time, float duration)
+        {
+            return time / duration;
+        }
+    }
+}
---
+++ Runtime/Tweening/TweeningUtils.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+namespace Utils.Tweening
+{
+    public static class TweeningUtils
+    {
+        public delegate float CurveFunction(float time, float duration);
+        
+        public static async Task TweenMoveAsync(
+            this Transform transform,
+            Vector3 from,
+            Vector3 to,
+            float duration,
+            CurveFunction curveFunction)
+        {
+            float startTime = Time.time;
+            float timeToFinish = startTime + duration;
+            Vector3 newPosition;
+            while (Time.time <= timeToFinish)
+            {
+                float time = Time.time - startTime;
+                float normalizedTime = curveFunction(time, duration);
+                newPosition = Vector3.Lerp(from, to, normalizedTime);
+                transform.position = newPosition;
+
+                await Task.Yield();
+            }
+
+            transform.position = to;
+        }
+    }
+}
