540b4084caf14ea453c54a6499e96328137f487d
mnicolas94 <miguelcok27@gmail.com>
2022-05-26 08:59:20 -0400

    FEAT(serializables): add ISerializablePredicate with more input types counts

--- Runtime/Serializables/ISerializablePredicate.cs
+++ Runtime/Serializables/ISerializablePredicate.cs
@@ -1,7 +1,32 @@
 ﻿﻿namespace Utils.Serializables
 {
+    public interface ISerializablePredicate
+    {
+        bool IsMet();
+    }
+    
     public interface ISerializablePredicate<T>
     {
         bool IsMet(T input);
+    }
+    
+    public interface ISerializablePredicate<T1, T2>
+    {
+        bool IsMet(T1 i1, T2 i2);
+    }
+    
+    public interface ISerializablePredicate<T1, T2, T3>
+    {
+        bool IsMet(T1 i1, T2 i2, T3 i3);
+    }
+    
+    public interface ISerializablePredicate<T1, T2, T3, T4>
+    {
+        bool IsMet(T1 i1, T2 i2, T3 i3, T4 i4);
+    }
+    
+    public interface ISerializablePredicate<T1, T2, T3, T4, T5>
+    {
+        bool IsMet(T1 i1, T2 i2, T3 i3, T4 i4, T5 i5);
     }
 }
