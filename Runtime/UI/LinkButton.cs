d0fc8a6476759fa6d0acab51eba78c33d7a9d5df
mnicolas94 <miguelcok27@gmail.com>
2022-06-18 22:26:03 -0400

    FEAT(UI): add button link

---
+++ Runtime/UI/LinkButton.cs
@@ -0,0 +1,22 @@
+﻿﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class LinkButton : MonoBehaviour
+    {
+        [SerializeField] private string link;
+        [SerializeField] private Button button;
+
+        private void Start()
+        {
+            button.onClick.AddListener(GoToLink);
+        }
+
+        private void GoToLink()
+        {
+            Application.OpenURL(link);
+        }
+    }
+}
