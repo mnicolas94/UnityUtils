4d362f175362224e110276b3e4eadd83766ba279
mnicolas94 <miguelcok27@gmail.com>
2022-08-19 09:31:46 -0400

    FEAT: implemet AutoProperty attribute

---
+++ Editor/AttributeDrawers/AutoPropertyDrawer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Utils.Attributes;
+using Object = UnityEngine.Object;
+
+namespace Utils.Editor.AttributeDrawers
+{
+    /// <summary>
+    /// Some code was taken from: https://github.com/Deadcows/MyBox
+    /// </summary>
+    [CustomPropertyDrawer(typeof(AutoPropertyAttribute))]
+    public class AutoPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property,
+            GUIContent label)
+        {
+            bool isUnityObject = true;
+            bool isEmpty = property.objectReferenceValue == null;
+            if (isUnityObject && isEmpty)
+            {
+                var att = (AutoPropertyAttribute) attribute;
+                var mode = att.Mode;
+                Func<Object, bool> predicateMethod = GetPredicateMethod(property, att);
+                var objects = GetObjectsFromAutoPropertyMode(mode, property, predicateMethod, fieldInfo.FieldType);
+                var obj = objects.FirstOrDefault();
+                if (obj != null)
+                {
+                    property.objectReferenceValue = obj;
+                    return;
+                }
+            }
+                    
+            EditorGUI.PropertyField(position, property, label);
+        }
+
+        private static Func<Object, bool> GetPredicateMethod(SerializedProperty property, AutoPropertyAttribute att)
+        {
+            return att.PredicateMethodTarget == null ?
+                att.PredicateMethodName == null ?
+                    _ => true :
+                    (Func<Object, bool>)Delegate.CreateDelegate(typeof(Func<Object, bool>),
+                        property.serializedObject.targetObject,
+                        att.PredicateMethodName) :
+                (Func<Object, bool>)Delegate.CreateDelegate(typeof(Func<Object, bool>),
+                    att.PredicateMethodTarget,
+                    att.PredicateMethodName);
+        }
+
+        private static IEnumerable<Object> GetObjectsFromAutoPropertyMode(
+            AutoPropertyMode mode,
+            SerializedProperty property,
+            Func<Object, bool> predicate,
+            Type type
+        )
+        {
+            IEnumerable<Object> objects;
+            switch (mode)
+            {
+                case AutoPropertyMode.Children:
+                    objects = ((Component) property.serializedObject.targetObject)
+                        ?.GetComponentsInChildren(type, true);
+                    break;
+                case AutoPropertyMode.Parent:
+                    objects = ((Component) property.serializedObject.targetObject)
+                        ?.GetComponentsInParent(type, true);
+                    break;
+                case AutoPropertyMode.Scene:
+                    objects = GetAllComponentsInSceneOf(property.serializedObject.targetObject, type);
+                    break;
+                case AutoPropertyMode.Asset:
+                    objects = AssetDatabase.FindAssets($"t:{type.Name}")
+                        .Select(AssetDatabase.GUIDToAssetPath)
+                        .Select(AssetDatabase.LoadAssetAtPath<Object>);
+                    break;
+                default:
+                case AutoPropertyMode.Any:
+                    var sceneObjects = GetAllComponentsInSceneOf(property.serializedObject.targetObject, type);
+                    var assets = AssetDatabase.FindAssets($"t:{type.Name}")
+                        .Select(AssetDatabase.GUIDToAssetPath)
+                        .Select(AssetDatabase.LoadAssetAtPath<Object>);
+                    objects = sceneObjects.Concat(assets);
+                    break;
+            }
+            
+            return objects.Where(predicate);
+        }
+            
+        /// <summary>
+        /// Get all Components in the same scene as a specified GameObject,
+        /// including inactive components.
+        /// 
+        /// source: https://github.com/Deadcows/MyBox/blob/master/Extensions/EditorExtensions/MyEditor.cs
+        /// </summary>
+        public static IEnumerable<Component> GetAllComponentsInSceneOf(Object obj,
+            Type type)
+        {
+            GameObject contextGO;
+            if (obj is Component comp) contextGO = comp.gameObject;
+            else if (obj is GameObject go) contextGO = go;
+            else return Array.Empty<Component>();
+            if (contextGO.scene.isLoaded) return contextGO.scene.GetRootGameObjects()
+                .SelectMany(rgo => rgo.GetComponentsInChildren(type, true));
+            return Array.Empty<Component>();
+        }
+    }
+}
---
+++ Runtime/Attributes/AutoProperty.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils.Attributes
+{
+	/// <summary>
+	/// Automatically assign components to this Property.
+	/// It searches for components from this GO or its children by default.
+	/// Pass in an <c>AutoPropertyMode</c> to override this behaviour.
+	/// <para></para>
+	/// Advanced usage: Filter found objects with a method. To do that, create a 
+	/// static method or member method of the current class with the same method
+	/// signature as a Func&lt;UnityEngine.Object, bool&gt;. Your predicate method
+	/// can be private.
+	/// If your predicate method is a member method of the current class, pass in
+	/// the nameof that method as the second argument.
+	/// If your predicate method is a static method, pass in the typeof class that
+	/// contains said method as the third argument.
+	///
+	/// source: https://github.com/Deadcows/MyBox
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field)]
+	public class AutoPropertyAttribute : PropertyAttribute
+	{
+		public readonly AutoPropertyMode Mode;
+		public readonly string PredicateMethodName;
+		public readonly Type PredicateMethodTarget;
+
+		public AutoPropertyAttribute(AutoPropertyMode mode = AutoPropertyMode.Children,
+			string predicateMethodName = null,
+			Type predicateMethodTarget = null)
+		{
+			Mode = mode;
+			PredicateMethodTarget = predicateMethodTarget;
+			PredicateMethodName = predicateMethodName;
+		}
+	}
+
+	public enum AutoPropertyMode
+	{
+		/// <summary>
+		/// Search for Components from this GO or its children.
+		/// </summary>
+		Children = 0,
+		/// <summary>
+		/// Search for Components from this GO or its parents.
+		/// </summary>
+		Parent = 1,
+		/// <summary>
+		/// Search for Components from this GO's current scene.
+		/// </summary>
+		Scene = 2,
+		/// <summary>
+		/// Search for Objects from this project's asset folder.
+		/// </summary>
+		Asset = 3,
+		/// <summary>
+		/// Search for Objects from anywhere in the project.
+		/// Combines the results of Scene and Asset modes.
+		/// </summary>
+		Any = 4
+	}
+}
