360c3038514545ae4814d595bdd19955c71f6fa6
mnicolas94 <miguelcok27@gmail.com>
2023-03-23 09:17:55 -0400

    feat(ui): add ToggleEvents component

---
+++ Runtime/UI/ToggleEvents.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Utils.UI
+{
+    public class ToggleEvents : MonoBehaviour
+    {
+        [SerializeField] private Toggle _toggle;
+        [SerializeField] private UnityEvent _onTrue;
+        [SerializeField] private UnityEvent _onFalse;
+
+        private void Start()
+        {
+            _toggle.onValueChanged.AddListener(OnValueChanged);
+            OnValueChanged(_toggle.isOn);
+        }
+
+        private void OnValueChanged(bool value)
+        {
+            if (value)
+            {
+                _onTrue.Invoke();
+            }
+            else
+            {
+                _onFalse.Invoke();
+            }
+        }
+    }
+}
