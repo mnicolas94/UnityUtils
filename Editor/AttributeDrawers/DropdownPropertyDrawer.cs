7ded5fa865433fffb21c40cbd95e2918d4688703
mnicolas94 <miguelcok27@gmail.com>
2023-03-31 12:23:16 -0400

    fix: set hotControl to 0 to avoid weird issue inside editor windows

--- Editor/AttributeDrawers/DropdownPropertyDrawer.cs
+++ Editor/AttributeDrawers/DropdownPropertyDrawer.cs
@@ -201,6 +201,7 @@ namespace Utils.Editor.AttributeDrawers
                 // TODO: Problem with structs, because they are value type.
                 // The solution is to make boxing/unboxing but unfortunately I don't know the compile time type of the target object
                 dropdownField.SetValue(target, newValue);
+                GUIUtility.hotControl = 0;
             }
         }
     }
