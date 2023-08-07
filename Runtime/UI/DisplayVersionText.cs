0196dc379e7fcee467c51165a8ea91dd9f063d56
mnicolas94 <miguelcok27@gmail.com>
2023-08-07 16:09:16 -0400

    feat: add DisplayVersionText script

---
+++ Runtime/UI/DisplayVersionText.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Utils.UI
+{
+    public class DisplayVersionText : MonoBehaviour
+    {
+        [Multiline(4)]
+        private string _versionFormatString = "game v{v}";
+        private TMP_Text _text;
+
+        void Start()
+        {
+            _text.text = GetFormattedString(_versionFormatString);
+        }
+        
+        public string GetFormattedString(string format)
+        {
+            var version = Application.version;
+            var outString = format.Replace("{v}", version);
+            outString = outString.Replace("{v_}", version.Replace('.', '_'));
+            // outString = outString.Replace("{gitHash}", Version.Instance.GitHash);
+            // outString = outString.Replace("{time}", Version.Instance.BuildTimestamp);
+
+            return outString;
+        }
+    }
+}
