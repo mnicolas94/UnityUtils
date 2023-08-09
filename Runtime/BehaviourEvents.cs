418c5463c0544aed9add84e3f59ab7af49aa319f
mnicolas94 <miguelcok27@gmail.com>
2023-08-09 12:59:21 -0400

    feat: add BehaviourEvents class

---
+++ Runtime/BehaviourEvents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Utils
+{
+    public class BehaviourEvents : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent _onAwake;
+        [SerializeField] private UnityEvent _onStart;
+        [SerializeField] private UnityEvent _onEnable;
+        [SerializeField] private UnityEvent _onDisable;
+        [SerializeField] private UnityEvent _onDestroy;
+
+        private void Awake()
+        {
+            _onAwake.Invoke();
+        }
+
+        private void Start()
+        {
+            _onStart.Invoke();
+        }
+
+        private void OnEnable()
+        {
+            _onEnable.Invoke();
+        }
+
+        private void OnDisable()
+        {
+            _onDisable.Invoke();
+        }
+
+        private void OnDestroy()
+        {
+            _onDestroy.Invoke();
+        }
+    }
+}
