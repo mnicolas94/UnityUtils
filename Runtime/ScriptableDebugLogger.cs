8a57e9ba4c243b3db23065787cf0047e49cc7bb3
mnicolas94 <miguelcok27@gmail.com>
2023-08-11 12:35:59 -0400

    feat: implement ScriptableDebugLogger

---
+++ Runtime/ScriptableDebugLogger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    [CreateAssetMenu(fileName = "DebugLogger", menuName = "Facticus/Utils/DebugLogger", order = 0)]
+    public class ScriptableDebugLogger : ScriptableObject
+    {
+        public void DebugMessage(string message)
+        {
+            Debug.Log(message);
+        }
+    }
+}
