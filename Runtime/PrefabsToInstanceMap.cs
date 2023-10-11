ddce06289eec9c1c330c27f140f47de2b2eff114
mnicolas94 <miguelcok27@gmail.com>
2023-10-11 15:24:06 -0400

    feat: implmement PrefabsToInstanceMap utility class

---
+++ Runtime/PrefabsToInstanceMap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Utils
+{
+    public class PrefabsToInstanceMap
+    {
+        private Dictionary<Object, Object> _instances = new Dictionary<Object, Object>();
+
+        public Object GetOrCreateInstance(Object prefab)
+        {
+            if (!_instances.ContainsKey(prefab))
+            {
+                var instance = Object.Instantiate(prefab);
+                _instances.Add(prefab, instance);
+
+                return instance;
+            }
+            else
+            {
+                return _instances[prefab];
+            }
+        }
+
+        public T GetOrCreateInstance<T>(Object prefab) where T : Object
+        {
+            return (T) GetOrCreateInstance(prefab);
+        }
+
+        public List<Object> GetAllInstances()
+        {
+            return _instances.Values.ToList();
+        }
+
+        public List<T> GetAllInstancesOfType<T>() where T : Object
+        {
+            return GetAllInstances().FindAll(instance => instance is T).ConvertAll(instance => (T) instance);
+        }
+
+        public void RemoveAndDestroyInstance(Object prefab)
+        {
+            if (_instances.ContainsKey(prefab))
+            {
+                var instance = _instances[prefab];
+                _instances.Remove(prefab);
+                Object.Destroy(instance);
+            }
+        }
+    }
+}
---
+++ Samples/PrefabToInstancesMap/PrefabsToInstanceTest.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils.Samples.PrefabToInstancesMap
+{
+    public class PrefabsToInstanceTest : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> _goPrefabs;
+        [SerializeField] private List<Transform> _transformPrefabs;
+        
+        private PrefabsToInstanceMap _map = new PrefabsToInstanceMap();
+
+        [ContextMenu(nameof(InstantiateAllGos))]
+        public void InstantiateAllGos()
+        {
+            foreach (var goPrefab in _goPrefabs)
+            {
+                _map.GetOrCreateInstance<GameObject>(goPrefab);
+            }
+        }
+
+        [ContextMenu(nameof(RemoveAllGos))]
+        public void RemoveAllGos()
+        {
+            foreach (var goPrefab in _goPrefabs)
+            {
+                _map.RemoveAndDestroyInstance(goPrefab);
+            }
+        }
+        
+        [ContextMenu(nameof(EnableAllGos))]
+        public void EnableAllGos()
+        {
+            foreach (var goPrefab in _goPrefabs)
+            {
+                var inst = _map.GetOrCreateInstance<GameObject>(goPrefab);
+                inst.SetActive(true);
+            }
+        }
+        
+        [ContextMenu(nameof(DisableAllGos))]
+        public void DisableAllGos()
+        {
+            foreach (var goPrefab in _goPrefabs)
+            {
+                var inst = _map.GetOrCreateInstance<GameObject>(goPrefab);
+                inst.SetActive(false);
+            }
+        }
+        
+        [ContextMenu(nameof(InstantiateAllTransforms))]
+        public void InstantiateAllTransforms()
+        {
+            foreach (var goPrefab in _transformPrefabs)
+            {
+                _map.GetOrCreateInstance<Transform>(goPrefab);
+            }
+        }
+
+        [ContextMenu(nameof(RemoveAllTransforms))]
+        public void RemoveAllTransforms()
+        {
+            foreach (var goPrefab in _transformPrefabs)
+            {
+                _map.RemoveAndDestroyInstance(goPrefab);
+            }
+        }
+        
+        [ContextMenu(nameof(EnableAllTransforms))]
+        public void EnableAllTransforms()
+        {
+            foreach (var goPrefab in _transformPrefabs)
+            {
+                var inst = _map.GetOrCreateInstance<Transform>(goPrefab);
+                inst.gameObject.SetActive(true);
+            }
+        }
+        
+        [ContextMenu(nameof(DisableAllTransforms))]
+        public void DisableAllTransforms()
+        {
+            foreach (var goPrefab in _transformPrefabs)
+            {
+                var inst = _map.GetOrCreateInstance<Transform>(goPrefab);
+                inst.gameObject.SetActive(false);
+            }
+        }
+    }
+}
