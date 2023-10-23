98d6f7d87bdd80f8b8419d4ddb252a785f900be2
mnicolas94 <miguelcok27@gmail.com>
2023-10-23 08:42:00 -0400

    feat: add FpsTargetSetter class

---
+++ Runtime/Behaviours/FpsTargetSetter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Utils.Behaviours
+{
+    public class FpsTargetSetter : MonoBehaviour
+    {
+        [SerializeField] private int _targetFps;
+
+        private void Start()
+        {
+            SetFps(_targetFps);
+        }
+
+        public void SetFps(int targetFps)
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = targetFps;
+        }
+    }
+}
