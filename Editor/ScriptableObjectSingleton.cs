f8ae5d218d61f3c30c20d286116b84c072749149
mnicolas94 <miguelcok27@gmail.com>
2024-05-22 14:19:27 -0400

    feat: add ScriptableObjectSingleton for editor

---
+++ Editor/ScriptableObjectSingleton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Utils.Editor
+{
+    public class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObjectSingleton<T>
+    {
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (IsNull)
+                {
+                    _instance = Load();
+                }
+                return _instance;
+            }
+        }
+        
+        private static bool IsNull => _instance == null;
+
+        private void OnEnable()
+        {
+            _instance = (T) this;
+            OnEnableCallback();
+        }
+
+        private void OnDisable()
+        {
+            _instance = null;
+            OnDisableCallback();
+        }
+        
+        protected virtual void OnEnableCallback(){}
+        protected virtual void OnDisableCallback(){}
+
+        private static T Load()
+        {
+            var assets = AssetDatabase.FindAssets($"t:{typeof(T).Name}");
+            
+            if (assets.Length == 0)
+            {
+                Debug.LogWarning($"Could not load any singleton object of type {typeof(T)}");
+                return null;
+            }
+            else if (assets.Length > 1)
+            {
+                Debug.LogWarning($"!!! Multiple instances of singleton type {typeof(T)} found");
+            }
+
+            string assetPath = AssetDatabase.GUIDToAssetPath(assets[0]);
+            var asset = AssetDatabase.LoadAssetAtPath<T>(assetPath);
+
+            return asset;
+        }
+    }
+}
