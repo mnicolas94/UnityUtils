26ee34d2ca3e5490b2df1de54668528142aa5e1b
mnicolas94 <miguelcok27@gmail.com>
2024-06-21 08:18:17 -0400

    feat: add UI/MaskInverter

---
+++ Runtime/UI/MaskInverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+namespace Common.UI
+{
+    /// <summary>
+    /// credits to: https://github.com/dreamcodestudio/UIMask
+    /// </summary>
+    public sealed class MaskInverter : MonoBehaviour, IMaterialModifier
+    {
+        private static readonly int _stencilComp = Shader.PropertyToID("_StencilComp");
+
+        public Material GetModifiedMaterial(Material baseMaterial)
+        {
+            var resultMaterial = new Material(baseMaterial);
+            resultMaterial.SetFloat(_stencilComp, (int)CompareFunction.NotEqual);
+            return resultMaterial;
+        }
+    }
+}
