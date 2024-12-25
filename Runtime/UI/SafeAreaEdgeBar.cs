96270b3c0268077a34a69401b52866e3fb3d2aa4
mnicolas94 <miguelcok27@gmail.com>
2024-12-25 16:11:25 -0500

    feat: implement SafeAreaEdgeBar.cs and SafeAreaRectTransform.cs

---
+++ Runtime/UI/SafeAreaEdgeBar.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Utils.Attributes;
+
+namespace Utils.UI
+{
+    public class SafeAreaEdgeBar : MonoBehaviour
+    {
+        [SerializeField, AutoProperty] private RectTransform _target;
+        public enum Edge { Top, Bottom, Left, Right }
+        [SerializeField] private Edge _edge;
+        
+        private void Start()
+        {
+            ResizeToSafeArea();
+        }
+
+        private void ResizeToSafeArea()
+        {
+            var rect = Screen.safeArea;
+
+            switch (_edge)
+            {
+                case Edge.Top:
+                    _target.anchorMin = new Vector2(rect.min.x / Screen.width, rect.max.y / Screen.height);
+                    _target.anchorMax = Vector2.one;
+                    break;
+                case Edge.Bottom:
+                    _target.anchorMin = Vector2.zero;
+                    _target.anchorMax = new Vector2(rect.max.x / Screen.width, rect.min.y / Screen.height);
+                    break;
+                case Edge.Left:
+                    _target.anchorMin = Vector2.zero;
+                    _target.anchorMax = new Vector2(rect.min.x / Screen.width, 1);
+                    break;
+                case Edge.Right:
+                    _target.anchorMin = new Vector2(rect.max.x / Screen.width, 0);
+                    _target.anchorMax = Vector2.one;
+                    break;
+            }
+        }
+    }
+}
---
+++ Runtime/UI/SafeAreaRectTransform.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using Utils.Attributes;
+
+namespace Utils.UI
+{
+    public class SafeAreaRectTransform : MonoBehaviour
+    {
+        [SerializeField, AutoProperty] private RectTransform _target;
+        
+        private void Start()
+        {
+            TransformToSafeArea();
+        }
+
+        private void TransformToSafeArea()
+        {
+            var safeRect = Screen.safeArea;
+
+            var anchorMin = new Vector2(
+                safeRect.min.x + _target.anchorMin.x * safeRect.width,
+                safeRect.min.y + _target.anchorMin.y * safeRect.height);
+            var anchorMax = new Vector2(
+                safeRect.min.x + _target.anchorMax.x * safeRect.width,
+                safeRect.min.y + _target.anchorMax.y * safeRect.height);
+
+            anchorMin.x /= Screen.width;
+            anchorMin.y /= Screen.height;
+            anchorMax.x /= Screen.width;
+            anchorMax.y /= Screen.height;
+
+            _target.anchorMin = anchorMin;
+            _target.anchorMax = anchorMax;
+        }
+    }
+}
