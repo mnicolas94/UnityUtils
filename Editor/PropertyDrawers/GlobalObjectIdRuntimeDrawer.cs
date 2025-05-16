9c1b1b19f34747c32418c9564a4089df26adbc00
mnicolas94 <miguelcok27@gmail.com>
2025-05-16 10:28:15 -0400

    refactor: move GlobalObjectIdRuntime.cs to com.facticus.utils package

---
+++ Editor/PropertyDrawers/GlobalObjectIdRuntimeDrawer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Utils.Editor.PropertyDrawers
+{
+    [CustomPropertyDrawer(typeof(GlobalObjectIdRuntime))]
+    public class GlobalObjectIdRuntimeDrawer : PropertyDrawer
+    {
+        private static readonly GUIContent MultipleNotSupportedLabel = new GUIContent("Multiple edit unsupported");
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.serializedObject.targetObjects.Length > 1)
+            {
+                EditorGUI.LabelField(position, MultipleNotSupportedLabel);
+                return;
+            }
+            
+            var idProperty = property.FindPropertyRelative("_id");
+            
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUI.PropertyField(position, idProperty, label);
+            EditorGUI.EndDisabledGroup();
+            
+            var id = GlobalObjectId.GetGlobalObjectIdSlow(property.serializedObject.targetObject);
+            EditorGUI.BeginChangeCheck();
+            idProperty.stringValue = id.ToString();
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.serializedObject.ApplyModifiedPropertiesWithoutUndo();
+            }
+        }
+
+    }
+}
---
+++ Runtime/GlobalObjectIdRuntime.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    [Serializable]
+    public class GlobalObjectIdRuntime
+    {
+        [SerializeField] private string _id;
+        public string ID => _id;
+
+        protected bool Equals(GlobalObjectIdRuntime other)
+        {
+            return _id == other._id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((GlobalObjectIdRuntime)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_id != null ? _id.GetHashCode() : 0);
+        }
+    }
+}
